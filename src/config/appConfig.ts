5a3cc44223f45b9f58e0eadae7fa2dae45aa219f
Jonathas Borges <jbc@icomp.ufam.edu.br>
2023-03-12 00:11:19 -0400

    [chore] whatsapp group url

--- src/config/appConfig.ts
+++ src/config/appConfig.ts
@@ -1,6 +1,6 @@
 // eslint-disable-next-line import/no-anonymous-default-export
 export default {
    url:{
-      whatsapp: "https://chat.whatsapp.com/C5F73TzIDl4Bk2G7vlze64",
+      whatsapp: "https://chat.whatsapp.com/EfuycpTBB0NJlKMKJwFHhN",
    },
 };
